a5cfe0ae93968380678feb557ee2fbd5509fc9a5
Alexander Marinov <sw1m3r330@gmail.com>
2020-12-20 18:38:18 -0800

    alex left button

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 console.log("TEST");
 
 const ctx = document.getElementById('canvas').getContext('2d');
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 10, 10);
+
+var x = 300
+
+ctx.fillStyle = 'blue';
+ctx.fillRect(x, 200, 100, 100);
+
+function moveLeft() {
+    console.log("Moving Left!");
+
+    ctx.fillStyle = 'white  ';
+    ctx.fillRect(x, 200, 100, 100);     
+
+    x = x - 10;
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(x, 200, 100, 100);
+}
+
+
+
+function moveRight() {
+    ctx.fillStyle = 'white  ';
+    ctx.fillRect(x, 200, 100, 100);
+
+    x = x + 10;
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(x, 200, 100, 100); 
+}
