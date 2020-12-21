ad71e4fcd6d33557b337e1e4d56891bd9f08ca03
Alexander Marinov <sw1m3r330@gmail.com>
2020-12-21 12:01:32 -0800

    added Up function

--- script.js
+++ script.js
@@ -2,31 +2,40 @@ console.log("TEST");
 
 const ctx = document.getElementById('canvas').getContext('2d');
 
-var x = 300
+var x = 300;
+var y = 300;
 
 ctx.fillStyle = 'blue';
-ctx.fillRect(x, 200, 100, 100);
+ctx.fillRect(x, y, 100, 100);
 
 function moveLeft() {
     console.log("Moving Left!");
 
     ctx.fillStyle = 'white  ';
-    ctx.fillRect(x, 200, 100, 100);     
+    ctx.fillRect(x, y, 100, 100);     
 
     x = x - 10;
 
     ctx.fillStyle = 'blue';
-    ctx.fillRect(x, 200, 100, 100);
+    ctx.fillRect(x, y, 100, 100);
 }
 
-
-
 function moveRight() {
-    ctx.fillStyle = 'white  ';
-    ctx.fillRect(x, 200, 100, 100);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(x, y, 100, 100);
 
     x = x + 10;
 
     ctx.fillStyle = 'blue';
-    ctx.fillRect(x, 200, 100, 100); 
+    ctx.fillRect(x, y, 100, 100); 
+} 
+
+function moveUp() { 
+    ctx.fillStyle = 'white';
+    ctx.fillRect(x, y, 100, 100);
+
+    y = y - 10;
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(x, y, 100, 100); 
 }
